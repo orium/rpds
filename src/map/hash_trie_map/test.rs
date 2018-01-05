7b476c330197a0268344154ba90e10bd30131206
idupree <antispam@idupree.com>
2018-01-05 19:12:11 +0000

    implement serde for HashTrieMap, and a test for it

--- src/map/hash_trie_map/test.rs
+++ src/map/hash_trie_map/test.rs
@@ -962,3 +962,13 @@ fn test_clone() -> () {
     assert_eq!(clone.get("hello"), Some(&4));
     assert_eq!(clone.get("there"), Some(&5));
 }
+
+#[cfg(feature = "serde")]
+#[test]
+fn test_serde() {
+    use bincode::{serialize, deserialize, Bounded};
+    let hashtriemap: HashTrieMap<i32, i32> = HashTrieMap::from_iter(vec![(5,6),(7,8),(9,10),(11,12)].into_iter());
+    let encoded = serialize(&hashtriemap, Bounded(100)).unwrap();
+    let decoded: HashTrieMap<i32, i32> = deserialize(&encoded).unwrap();
+    assert_eq!(hashtriemap, decoded);
+}
