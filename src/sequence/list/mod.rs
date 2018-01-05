fd6c77ea53b11ec388b6a1cdd9de256edcbba408
idupree <antispam@idupree.com>
2018-01-05 17:34:07 +0000

    implement serde for List, and a test for it

--- src/sequence/list/mod.rs
+++ src/sequence/list/mod.rs
@@ -10,6 +10,15 @@ use std::hash::{Hasher, Hash};
 use std::borrow::Borrow;
 use std::iter::FromIterator;
 
+#[cfg(feature = "serde")]
+use serde::ser::{Serialize, Serializer, SerializeSeq};
+#[cfg(feature = "serde")]
+use serde::de::{Deserialize, Deserializer, SeqAccess, Visitor};
+#[cfg(feature = "serde")]
+use std::marker::PhantomData;
+#[cfg(feature = "serde")]
+use std::fmt;
+
 // TODO Use impl trait instead of this when available.
 pub type Iter<'a, T> = ::std::iter::Map<IterArc<'a, T>, fn(&Arc<T>) -> &T>;
 
@@ -266,6 +275,64 @@ impl<'a, T> Iterator for IterArc<'a, T> {
 }
 
 impl<'a, T> ExactSizeIterator for IterArc<'a, T> {}
+
+#[cfg(feature = "serde")]
+impl<T> Serialize for List<T> where T: Serialize {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut state = serializer.serialize_seq(Some(self.len()))?;
+        for item in self {
+            state.serialize_element(&item)?;
+        }
+        state.end()
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'de, T> Deserialize<'de> for List<T> where T: Deserialize<'de> {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        deserializer.deserialize_seq(ListVisitor{phantom: PhantomData})
+    }
+}
+
+#[cfg(feature = "serde")]
+struct ListVisitor<T> {
+    phantom: PhantomData<T>
+}
+
+#[cfg(feature = "serde")]
+impl<'de, T> Visitor<'de> for ListVisitor<T>
+where T: Deserialize<'de>,
+{
+    type Value = List<T>;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a sequence")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+        where
+            A: SeqAccess<'de>,
+    {
+        let mut vec: Vec<T> = if let Some(capacity) = seq.size_hint() {
+                Vec::with_capacity(capacity)
+            } else {
+                Vec::new()
+            };
+
+        while let Some(value) = seq.next_element()? {
+            vec.push(value);
+        }
+
+        let mut list: List<T> = List::new();
+
+        for value in vec.into_iter().rev() {
+            list = list.push_front(value);
+        }
+
+        Ok(list)
+    }
+}
+
 
 #[cfg(test)]
 mod test;
