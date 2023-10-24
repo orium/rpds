8dd589a236f78a44677e8e1441e376064a77db5d
Michael Sproul <michael@sigmaprime.io>
2023-10-24 23:22:48 +1100

    Rough benchmarks (a feature might be better)

---
+++ benches/rpds_list_triomphe.rs
@@ -0,0 +1,172 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#![cfg_attr(feature = "fatal-warnings", deny(warnings))]
+
+use archery::ArcTK;
+use criterion::{criterion_group, criterion_main, Criterion};
+use rpds::List;
+use std::hint::black_box;
+
+fn rpds_list_triomphe_push_front(c: &mut Criterion) {
+    let limit = 10_000;
+
+    c.bench_function("rpds list sync push front", move |b| {
+        b.iter(|| {
+            let mut list: List<usize, ArcTK> = List::new_with_ptr_kind();
+
+            for i in 0..limit {
+                list = list.push_front(i);
+            }
+
+            list
+        })
+    });
+}
+
+fn rpds_list_triomphe_push_front_mut(c: &mut Criterion) {
+    let limit = 10_000;
+
+    c.bench_function("rpds list sync push front mut", move |b| {
+        b.iter(|| {
+            let mut list: List<usize, ArcTK> = List::new_with_ptr_kind();
+
+            for i in 0..limit {
+                list.push_front_mut(i);
+            }
+
+            list
+        })
+    });
+}
+
+fn rpds_list_triomphe_drop_first(c: &mut Criterion) {
+    let limit = 10_000;
+
+    c.bench_function("rpds list sync drop first", move |b| {
+        b.iter_with_setup(
+            || {
+                let mut list: List<usize, ArcTK> = List::new_with_ptr_kind();
+
+                for i in 0..limit {
+                    list.push_front_mut(i);
+                }
+
+                list
+            },
+            |mut list| {
+                for _ in 0..limit {
+                    list = list.drop_first().unwrap();
+                }
+
+                list
+            },
+        );
+    });
+}
+
+fn rpds_list_triomphe_drop_first_mut(c: &mut Criterion) {
+    let limit = 10_000;
+
+    c.bench_function("rpds list sync drop first mut", move |b| {
+        b.iter_with_setup(
+            || {
+                let mut list: List<usize, ArcTK> = List::new_with_ptr_kind();
+
+                for i in 0..limit {
+                    list.push_front_mut(i);
+                }
+
+                list
+            },
+            |mut list| {
+                for _ in 0..limit {
+                    list.drop_first_mut();
+                }
+
+                list
+            },
+        );
+    });
+}
+
+fn rpds_list_triomphe_reverse(c: &mut Criterion) {
+    let limit = 1_000;
+
+    c.bench_function("rpds list sync reverse", move |b| {
+        b.iter_with_setup(
+            || {
+                let mut list: List<usize, ArcTK> = List::new_with_ptr_kind();
+
+                for i in 0..limit {
+                    list.push_front_mut(i);
+                }
+
+                list
+            },
+            |mut list| {
+                for _ in 0..limit {
+                    list = list.reverse();
+                }
+
+                list
+            },
+        );
+    });
+}
+
+fn rpds_list_triomphe_reverse_mut(c: &mut Criterion) {
+    let limit = 1_000;
+
+    c.bench_function("rpds list sync reverse mut", move |b| {
+        b.iter_with_setup(
+            || {
+                let mut list: List<usize, ArcTK> = List::new_with_ptr_kind();
+
+                for i in 0..limit {
+                    list.push_front_mut(i);
+                }
+
+                list
+            },
+            |mut list| {
+                for _ in 0..limit {
+                    list.reverse_mut();
+                }
+
+                list
+            },
+        );
+    });
+}
+
+fn rpds_list_triomphe_iterate(c: &mut Criterion) {
+    let limit = 10_000;
+    let mut list: List<usize, ArcTK> = List::new_with_ptr_kind();
+
+    for i in 0..limit {
+        list.push_front_mut(i);
+    }
+
+    c.bench_function("rpds list sync iterate", move |b| {
+        b.iter(|| {
+            for i in list.iter() {
+                black_box(i);
+            }
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    rpds_list_triomphe_push_front,
+    rpds_list_triomphe_push_front_mut,
+    rpds_list_triomphe_drop_first,
+    rpds_list_triomphe_drop_first_mut,
+    rpds_list_triomphe_reverse,
+    rpds_list_triomphe_reverse_mut,
+    rpds_list_triomphe_iterate
+);
+criterion_main!(benches);
